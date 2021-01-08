f57622e7a0fabc134dfb9e4fdcc01dfd9da41dce
cindy <ciindy.wu@gmail.com>
2021-01-07 16:01:34 -1000

    Add validation for message form using bouncer library

--- src/clj/collector/routes/home.clj
+++ src/clj/collector/routes/home.clj
@@ -1,24 +1,38 @@
 (ns collector.routes.home
   (:require [collector.layout :as layout]
+            [bouncer.core :as b]
+            [bouncer.validators :as v]
             [compojure.core :refer [defroutes GET POST]]
             [ring.util.http-response :as response]
             [collector.db.core :as db]))
 
-(defn home-page []
+(defn home-page [{:keys [flash]}]
   (layout/render
-    "home.html" 
-    {:messages (db/get-messages)}))
-
-(defn save-message! [{:keys [params]}]
-  (db/save-message!
-   (assoc params :timestamp (java.util.Date.)))
-  (response/found "/"))
+   "home.html"
+   (merge {:messages (db/get-messages)}
+          (select-keys flash [:name :message :errors]))))
 
 (defn about-page []
   (layout/render "about.html"))
 
+(defn validate-message [params]
+  (first
+   (b/validate
+    params
+    :name v/required
+    :message [v/required [v/min-count 10]])))
+
+(defn save-message! [{:keys [params]}]
+  (if-let [errors (validate-message params)]
+    (-> (response/found "/")
+        (assoc :flash (assoc params :errors errors)))
+    (do
+      (db/save-message!
+       (assoc params :timestamp (java.util.Date.)))
+      (response/found "/"))))
+
 (defroutes home-routes
-  (GET "/" [] (home-page))
+  (GET "/" request (home-page request))
   (POST "/message" request (save-message! request))
   (GET "/about" [] (about-page)))
 
